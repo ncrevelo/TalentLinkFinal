ee299c0fd1525c09546d3aa4075fd6a4fb7b4d8b
Laptop <jctello@sura.com.co>
2025-09-20 11:58:56 -0500

    hirer dashboard with metrics complete

--- src/modules/jobs/hooks/useJobs.tsx
+++ src/modules/jobs/hooks/useJobs.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState, useEffect, useCallback } from 'react';
 import { QueryDocumentSnapshot, DocumentData } from 'firebase/firestore';
 import { jobService } from '../services';
